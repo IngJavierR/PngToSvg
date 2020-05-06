498b0def43a3c753a0d5595a4ccb37877a900160
Javier Rodriguez <francisco.rodriguez@axity.com>
2020-05-06 18:21:13 -0500

    First commit

---
+++ init.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from io import StringIO
+
+def svg_header(width, height):
+    return """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="%d" height="%d"
+     xmlns="http://www.w3.org/2000/svg" version="1.1">
+""" % (width, height)    
+
+def rgba_image_to_svg_pixels(im, opaque=None):
+    s = StringIO()
+    s.write(svg_header(*im.size))
+
+    width, height = im.size
+    for x in range(width):
+        for y in range(height):
+            here = (x, y)
+            rgba = im.getpixel(here)
+            if opaque and not rgba[3]:
+                continue
+            s.write("""  <rect x="%d" y="%d" width="1" height="1" style="fill:rgb%s; fill-opacity:%.3f; stroke:none;" />\n""" % (x, y, rgba[0:3], float(rgba[2]) / 255))
+    s.write("""</svg>\n""")
+    return s.getvalue()
+
+def main():
+    image = Image.open('examples/angular.png').convert('RGB')
+    print(rgba_image_to_svg_pixels(image))
+
+if __name__ == '__main__':
+    main()
